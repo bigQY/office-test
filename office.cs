2c85e46aba1ead1c0feecf39478c723d3c475a8a
zejxmin <52437374+bigQY@users.noreply.github.com>
2021-07-26 11:33:02 +0800

    Update office.cs

--- office.cs
+++ office.cs
@@ -51,4 +51,36 @@ func _on_AnimationPlayer_animation_finished( name ):
 	pass # replace with function body
 	print ("BREED complete in badges")
 	emit_signal("breed_complete", self) # null to fake tween completed
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
 }
